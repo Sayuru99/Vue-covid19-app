2a683b7bede6d24c7ef5b6e6ce05f1cdcc3fc1f8
Sayuru De Alwis <saurudealwis@ymail.com>
2022-04-18 20:12:27 +0530

    upload

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="grid md:grid-col-2 gap-4">
+        <!-- box 1  -->
+        <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{numberWithComas(stats.NewConfirmed)}}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{numberWithComas(stats.TotalConfirmed)}}
+            </div>
+        </div>
+
+        <!-- box 2  -->
+        <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{numberWithComas(stats.NewDeaths)}}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{numberWithComas(stats.TotalDeaths)}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'DataBoxes',
+    props: ['stats'],
+    methods: {
+        numberWithComas(x)  {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        }
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="text-center bg-zinc-900 text-neutral-300 p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa-solid fa-virus"></i>Covid-19 Summary</div>
+  </header>
+ 
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+
